6c899a9eca56d6c415059dd81af5b270ddf307d5
NoIdeaIndustry <bernier.charles.paris@gmail.com>
2022-03-30 15:07:33 +0200

    Revert "added light machine, everything working"
    This reverts commit a37378ce44b7aac46b87420168a3f7b3e62f71f5.

---
+++ Assets/Aurage/Scripts/LampStation.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampStation : MonoBehaviour
+{
+    [Header("Reference")]
+    private PowerManager refPowerManager;
+    public Lamp refLamp;
+
+    [Header("Lamp UI/UX")]
+    [HideInInspector] public MeshRenderer meshRenderer;
+
+    [Header("Lamp Stats")]
+    public float restorePower;
+    public bool canCharge = true;
+
+    private void Awake()
+    {
+        refPowerManager = GameObject.FindObjectOfType<PowerManager>();
+
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = refLamp.machineMaterials[1];
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(LightSwitchEvent());
+    }
+
+    private IEnumerator LightSwitchEvent()
+    {
+        refLamp.isMachinUsed = true;
+        RestorePowerEvent();
+
+        yield return new WaitForSeconds(.5f);
+        meshRenderer.material = refLamp.machineMaterials[0];
+        canCharge = false;
+    }
+
+    private void RestorePowerEvent()
+    {
+        refPowerManager.currentPower += restorePower;
+    }
+}
--- Assets/Aurage/Scripts/PowerManager.cs
+++ Assets/Aurage/Scripts/PowerManager.cs
@@ -5,50 +5,47 @@ using UnityEngine;
 public class PowerManager : MonoBehaviour
 {
     private GameObject refPlayer;
-    private ChargingStation refChargingStation;
-    private PowerBar refPowerBar;
+    private GeneratorStation refGeneratorStation;
 
     [Header("Charging Stats")]
-    public float unchargePower;
+    public float unchargePowerDelta;
     public bool isCharging = false;
 
     [Header("Player Stats")]
+    public bool outOfPower;
     public float maxPower;
     public float currentPower;
+    public Transform waypoint;
+
 
     private void Awake()
     {
         refPlayer = GameObject.FindGameObjectWithTag("Player");
-        refChargingStation = GameObject.FindObjectOfType<ChargingStation>();
-        refPowerBar = GameObject.FindObjectOfType<PowerBar>();
+        refGeneratorStation = GameObject.FindObjectOfType<GeneratorStation>();
 
         currentPower = maxPower;
     }
 
     private void Update()
     {
-        if(isCharging)
-            StartCoroutine(PluggedEvent(refChargingStation.chargingPower));
-        else
-            StartCoroutine(UnpluggedEvent(unchargePower));
+        if (outOfPower)
+            StartCoroutine(OnOutOfPowerEvent());
+
+        if (currentPower > maxPower)
+            currentPower = maxPower;
     }
 
-    private IEnumerator PluggedEvent(float powerAmount)
+    private IEnumerator OnOutOfPowerEvent()
     {
-        if (currentPower >= maxPower)
-            isCharging = false;
-
-        currentPower += powerAmount * 0.0001f;
+        refPlayer.GetComponent<MeshRenderer>().enabled = false;
         yield return new WaitForSecondsRealtime(2f);
-    }
 
-    private IEnumerator UnpluggedEvent(float powerAmount)
-    {
-        if (currentPower <= 0)
-            refPlayer.SetActive(false);
+        refPlayer.GetComponent<MeshRenderer>().enabled = true;
+        currentPower = maxPower;
+        outOfPower = false;
 
-        currentPower -= powerAmount * 0.0001f;
-        refPowerBar.SetLife(currentPower);
-        yield return new WaitForSecondsRealtime(2f);
+        refPlayer.transform.position = waypoint.position;
+        refGeneratorStation.RestoreGeneratorStateEvent();
     }
 }
+
