9330a79c5baedf51bc5866779edd3cd7a0a89daf
Yann DCHX <y.dechaux@student.isartdigital.com>
2022-04-01 16:04:52 +0200

    New Enemy Scene

--- Assets/Aurage/Scripts/BasicEnemy.cs
+++ Assets/Aurage/Scripts/BasicEnemy.cs
@@ -5,14 +5,38 @@ using UnityEngine;
 public class BasicEnemy : MonoBehaviour
 {
     // Start is called before the first frame update
+    bool isEdge = false;
+    bool isTurning = false;
+    Vector3 dir = Vector3.forward;
+    Rigidbody rigidbody;
+    LayerMask edge;
     void Start()
     {
-        
+        edge = 6;
     }
 
     // Update is called once per frame
+
     void Update()
     {
-        
+        DirectionGetter();
+
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+
+    }
+
+    private void DirectionGetter()
+    {
+        isEdge = Physics.Raycast(transform.position + dir * 0.8f, dir, 0.2f, edge);
+
+    }
+
+private void Move()
+    {
+        rigidbody.velocity = new Vector3(0,0,0);
     }
 }
