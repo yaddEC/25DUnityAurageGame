bd0d1cd3769d4f048500e6a602d06aaed7b6e2a6
Yann DCHX <y.dechaux@student.isartdigital.com>
2022-04-01 13:39:32 +0200

    Aiming system

---
+++ Assets/Aurage/Scripts/ElectricityBullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricityBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody rb;
+    public Vector3 moveDirection;
+    public float bulletSpeed=5;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = moveDirection.normalized;
+        rb.velocity = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z)*bulletSpeed;
+        Destroy(gameObject, 2f);
+    }
+}
--- Assets/Aurage/Scripts/Movement.cs
+++ Assets/Aurage/Scripts/Movement.cs
@@ -7,15 +7,23 @@ public class Movement : MonoBehaviour
 {
 
     
-    public GameObject aiming;
-    GameObject clone;
+    public GameObject aimingArrow;
+    public GameObject bullet;
+    GameObject aimingArrowClone;
+    GameObject bulletClone;
+
+    bool isAiming = false;
+    public Vector2 aimDir ;
     // Start is called before the first frame update
     void Start()
     {
-        
-
+        aimingArrowClone = Instantiate(aimingArrow, transform.position + Vector3.right, Quaternion.identity);
+        aimingArrowClone.transform.parent = gameObject.transform;
+        aimingArrowClone.transform.localScale = new Vector3(0, 0, 0);
+        aimDir = Vector2.right;
     }
 
+  
     // Update is called once per frame
     void Update()
     {
@@ -26,16 +34,51 @@ public class Movement : MonoBehaviour
 
     public void Aim(InputAction.CallbackContext value)
     {
-        if (value.performed)
-            clone = Instantiate(aiming, transform.position+Vector3.right, Quaternion.identity);
 
+        isAiming =true;
+        if (value.performed)
+        {
+            aimingArrowClone.transform.localScale = new Vector3(1, 1, 1);
+        }
+           
+       
+
+    }
+
+    public void LeftStick(InputAction.CallbackContext value)
+    {
+        if(isAiming)
+        {
+
+        if (value.performed)
+            {
+                aimDir = value.ReadValue<Vector2>();
+                
+                
+            }
+
+         
+            transform.localEulerAngles = new Vector3(0, 0, Mathf.Atan2(aimDir.x, aimDir.y) * -180 / Mathf.PI + 90f);
+
+        }
+        else
+        {
+            //Movement
+        }
     }
 
     public void Fire(InputAction.CallbackContext value)
     {
 
         if (value.performed)
-            Destroy(clone);
+        {
+            aimingArrowClone.transform.localScale = new Vector3(0, 0, 0);
+            isAiming = false;
+            bulletClone = Instantiate(bullet, transform.position , Quaternion.identity);
+            bulletClone.GetComponent<ElectricityBullet>().moveDirection = new Vector3(aimDir.x,aimDir.y,0);
+            
+        }
+
 
     }
 
