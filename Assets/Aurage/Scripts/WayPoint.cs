7a01379ff48911ca0f420574fde2c0c6bdd6c73a
Yann DCHX <y.dechaux@student.isartdigital.com>
2022-04-19 16:02:34 +0200

    Add Z axis and waypoints to basic Enemy

---
+++ Assets/Aurage/Scripts/WayPoint.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    private BasicEnemy refBasicEnemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "BasicEnemy")
+        {
+            refBasicEnemy = other.gameObject.GetComponent<BasicEnemy>();
+            refBasicEnemy.ChangeWayPoint(this.gameObject);
+        }
+    }
+
+}
