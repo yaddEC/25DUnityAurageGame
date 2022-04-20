da876ba090e989d084f2935a403c134060150d67
Robion Mathieu <m.robion@student.isartdigital.com>
2022-04-20 09:59:26 +0200

    The pause menu is almost finished, just set the main scene.

--- Assets/Aurage/Scripts/PauseMenu.cs
+++ Assets/Aurage/Scripts/PauseMenu.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int boolToInt(bool value)
     {
-        
+        if (value)
+            return 1;
+        return 0;
+    }
+    public void onActivate(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Pause");
+            this.gameObject.SetActive(!this.gameObject.activeSelf);
+            Time.timeScale = boolToInt(!this.gameObject.activeSelf);
+        }
+    }
+    public void onBackPresed()
+    {
+        this.gameObject.SetActive(!this.gameObject.activeSelf);
+        Time.timeScale = boolToInt(!this.gameObject.activeSelf);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void onRestartPresed()
     {
-        
+        KillZone.isDead = false;
+        Scene newScene = SceneManager.GetActiveScene();
+        SceneManager.UnloadSceneAsync(newScene.name);
+        SceneManager.LoadScene(newScene.name);
+        onBackPresed();
     }
 }
