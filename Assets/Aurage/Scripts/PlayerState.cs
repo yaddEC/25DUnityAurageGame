8d1fda4ed44e362b91a061b115a24faecad54906
Yann Dechaux <y.dechaux@student.isartdigital.com>
2022-04-23 16:46:54 +0200

    PlayerState and Islit for majordom

---
+++ Assets/Aurage/Scripts/PlayerState.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState : MonoBehaviour
+{
+    public static bool isVisible;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isVisible = true; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
